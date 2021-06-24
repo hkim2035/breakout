57a03e3549b3c3f7abd0d6f3e68b5117a993fef6
hkim2035 <egmont74@gmail.com>
2021-06-24 10:32:20 +0900

    stress field calculation, done.

--- BO.py
+++ BO.py
@@ -1,9 +1,8 @@
 import sys
 import os.path
 import math
-from zipfile import ZIP_DEFLATED
 import numpy as np
-from numpy.lib.npyio import zipfile_factory
+
 
 def rock_strength_parameter(fric, coh):
     Co = 2*coh*(math.sqrt(fric**2+1)+fric)  # uniaxial compressive strength
@@ -16,17 +15,46 @@ def rock_strength_parameter(fric, coh):
 
     return Co, T
 
-def pstress(r, zeta, S1, S2, S3, So):
-    
+# calculate principal stress field around wellbore
+def pstress(r, zeta):
+
     global SHmax, SHmin, Sv, nu, Pm, Po
 
     theta = 2.*zeta*3.141593/180.
     cos1 = math.cos(theta)
     sin1 = math.sin(theta)
 
-    So = 0.5*(SHmax + SHmin)*(1+r**2)
-    So = So - 0.5*(SHmax-SHmin)*(1+3*r**4)*cos1
+    So = 0.5*(SHmax+SHmin) * (1.+r**2)
+    So = So - 0.5*(SHmax-SHmin)*(1.+3*r**4)*cos1
     So = So - r**2*(Pm-Po)
+
+    Sr = 0.5*(SHmax+SHmin)*(1.-r**2)
+    Sr = Sr + 0.5*(SHmax-SHmin)*cos1*(1.-4*r**2+3*r**4)
+    Sr = Sr + r**2*(Pm-Po)
+    Sro = -0.5*(SHmax-SHmin)*sin1*(1.+2*r**2-3*r**4)
+    Sz = Sv - 2*nu*(SHmax-SHmin)*cos1*r**2
+    Sp1 = 0.5*(So+Sr)
+    Sp1 = Sp1 + 0.5*math.sqrt((So-Sr)**2+4*Sro**2)
+    Sp2 = 0.5*(So+Sr)
+    Sp2 = Sp2 - 0.5*math.sqrt((So-Sr)**2+4*Sro**2)
+
+    if Sp2 >= Sz: 
+        S1 = Sp1
+        S2 = Sp2
+        S3 = Sz
+
+    if (Sp1 >= Sz) & (Sz > Sp2):
+        S1= Sp1
+        S2 = Sz
+        S3 = Sp2
+
+    if Sz >= Sp1:
+        S1 = Sz
+        S2 = Sp1
+        S3 = Sp2
+
+    return S1, S2, S3
+
 
 sys.argv
 
@@ -72,43 +100,13 @@ Nrad3 = 6./drad
 r = [radius/yy for yy in rad22]
 
 # calculate stress field
-def pstress(r, zeta, Sl, S2, S3, So):
+pstress_distr = []
+for ir in r:
+    for izeta in zeta:
+        S1, S2, S3 = pstress(ir,izeta)
+        pstress_distr.append([ir,izeta, S1, S2, S3])
 
-    global SHmax, SHmin, Sv, nu, Pm, Po
-
-    theta = 2.*zeta*3.141593/180.
-    cos1 = math.cos(theta)
-    sin1 = math.sin(theta)
-
-    So = 0.5*(SHmax+SHmin) * (1.+r**2)
-    So = So - 0.5*(SHmax-SHmin)*(1.+3*r**4)*cos1
-    So = So - r**2*(Pm-Po)
-
-    Sr = 0.5*(SHmax+SHmin)*(1.-r**2)
-    Sr = Sr + 0.5*(SHmax-SHmin)*cos1*(1.-4*r**2+3*r**4)
-    Sr = Sr + r**2*(Pm-Po)
-    Sro = -0.5*(SHmax-SHmin)*sin1*(1.+2*r**2-3*r**4)
-    Sz = Sv - 2*nu*(SHmax-SHmin)*cos1*r**2
-    Sp1 = 0.5*(So+Sr)
-    Sp1 = Sp1 + 0.5*math.sqrt((So-Sr)**2+4*Sro**2)
-    Sp2 = 0.5*(So+Sr)
-    Sp2 = Sp2 - 0.5*math.sqrt((So-Sr)**2+4*Sro**2)
-
-    if Sp2 >= Sz: 
-        S1 = Sp1
-        S2 = Sp2
-        S3 = Sz
-
-    if (Sp1 >= Sz) & (Sz > Sp2):
-        S1= Sp1
-        S2 = Sz
-        S3 = Sp2
-
-    if Sz >= Sp1:
-        S1 = Sz
-        S2 = Sp1
-        S3 = Sp2
-
+breakpoint()
 # calculate rock stress
 
 
